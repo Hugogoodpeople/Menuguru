45d8ebe488f75cb7e447e1ce5341cb70e4c7fe3c
Hugo Costa <hugocosta@goodpeople.com>
2014-09-19 18:00:38 +0100

    mega alterações
    Já abrem os restaurantes… mas abre sempre o mesmo pro enquanto

--- Menuguru6/app/src/main/java/pt/menuguru/menuguru6/Restaurante_main.java
+++ Menuguru6/app/src/main/java/pt/menuguru/menuguru6/Restaurante_main.java
@@ -4,7 +4,6 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,10 +13,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,10 +25,7 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 import pt.menuguru.menuguru6.Json_parser.JSONParser;
-import pt.menuguru.menuguru6.Utils.Globals;
-import pt.menuguru.menuguru6.Utils.Locais;
 import pt.menuguru.menuguru6.Utils.Menu_do_restaurante;
-import pt.menuguru.menuguru6.Utils.Refugio_item;
 
 /**
  * Created by hugocosta on 19/09/14.
@@ -39,7 +35,7 @@ public class Restaurante_main extends Activity {
 
     private static MyListAdapter mAdapter;
     private ArrayList<Menu_do_restaurante> some_list;
-    private GridView gridView;
+    private ListView gridView;
     private ProgressDialog progressDialog;
 
     @Override
@@ -56,42 +52,7 @@ public class Restaurante_main extends Activity {
     }
 
 
-    public class MyListAdapter extends ArrayAdapter<Menu_do_restaurante> {
 
-        Context myContext;
-
-
-        public MyListAdapter(Context context, int textViewResourceId, ArrayList<Menu_do_restaurante> objects) {
-            super(context, textViewResourceId, objects);
-            myContext = context;
-        }
-
-        @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
-            //return super.getView(position, convertView, parent);
-
-            LayoutInflater inflater =(LayoutInflater)myContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View row = convertView;
-
-
-
-            if (row == null)
-                row=inflater.inflate(R.layout.row_inspiracao, parent, false);
-            TextView label2=(TextView)row.findViewById(R.id.textView_row_refugio);
-            label2.setText(some_list.get(position).getNome());
-
-
-
-            ImageView icon=(ImageView)row.findViewById(R.id.imageView_menu_refugio);
-
-            //icon.setImageResource(some_list.get(position).getImg());
-
-
-
-            return row;
-        }
-
-    }
 
 
     @Override
@@ -161,28 +122,33 @@ public class Restaurante_main extends Activity {
                 dict.put("horario_lang","pt");
                 dict.put("rest_id","477");
 
+                // tenho de enviar lat, long, data, hora, cidade, lang
+
                 String jsonString = jParser.getJSONFromUrl(yourJsonStringUrl,dict);
 
-                // get the array of users
+                Log.v("jfgrhng","resultado da procura = "+ jsonString);
 
-                /*
+
+                // try parse the string to a JSON object
                 try {
-                    jsonObj = new JSONObject(jsonString.substring(jsonString.indexOf("{"), jsonString.lastIndexOf("}") + 1));
+                    Log.v("Ver Json ","Ele retorna isto"+jsonString);
+                    jsonObj = new JSONObject(jsonString);
                 } catch (JSONException e) {
-                    Log.e("JSON Parser", "Error parsing data [" + e.getMessage() + "] " + jsonString);
+                    Log.e(TAG, "Error parsing data " + e.toString());
                 }
-                */
+                // get the array of users
 
                 dataJsonArr = jsonObj.getJSONArray("res");
 
-                Log.v("JsonObject","objecto = "+ jsonObj);
+                // loop through all users
+
 
                 some_list = new ArrayList<Menu_do_restaurante>();
-
-
                 for (int i = 0; i < dataJsonArr.length(); i++) {
+                    JSONObject c = dataJsonArr.getJSONObject(i);
 
                     Menu_do_restaurante menu = new Menu_do_restaurante();
+                    menu.setNome(c.getString("nome"));
 
                     some_list.add(menu);
                 }
@@ -206,13 +172,24 @@ public class Restaurante_main extends Activity {
         //mAdapter.notifyDataSetChanged();
         //mAdapter = new MyListAdapter(getApplicationContext(), R.layout.row_defenicoes, some_array);
 
-        gridView = (GridView) findViewById(R.id.lista_menus_restaurante);
+        gridView = (ListView) findViewById(R.id.lista_menus_restaurante);
         //adapter = new ArrayAdapter<Locais>(this,android.R.layout.simple_list_item_1, android.R.id.text1, local);
+
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        ViewGroup header2 = (ViewGroup) inflater.inflate(R.layout.header_restaurante_main, gridView, false);
+
+        gridView.addHeaderView(header2, null, false);
+
 
         // Assign adapter to ListView
         //listView.setAdapter(adapter);
-        mAdapter = new MyListAdapter(this, R.layout.row_defenicoes, some_list);
+        mAdapter = new MyListAdapter(this, R.layout.grid_menu, some_list);
         gridView.setAdapter(mAdapter);
+
+
+
         // ListView Item Click Listener
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
@@ -240,6 +217,42 @@ public class Restaurante_main extends Activity {
             }
 
         });
+
+    }
+
+
+
+
+    public class MyListAdapter extends ArrayAdapter<Menu_do_restaurante> {
+
+        Context myContext;
+
+
+        public MyListAdapter(Context context, int textViewResourceId, ArrayList<Menu_do_restaurante> objects) {
+            super(context, textViewResourceId, objects);
+            myContext = context;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            //return super.getView(position, convertView, parent);
+
+            LayoutInflater inflater =(LayoutInflater)myContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = convertView;
+
+
+
+            if (row == null)
+                row=inflater.inflate(R.layout.grid_menu, parent, false);
+            TextView label2=(TextView)row.findViewById(R.id.lista_menus_restaurante);
+            label2.setText(some_list.get(position).getNome());
+
+            ImageView icon=(ImageView)row.findViewById(R.id.imageView_menu_refugio);
+
+            //icon.setImageResource(some_list.get(position).getImg());
+
+            return row;
+        }
 
     }
 
